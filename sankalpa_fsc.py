04f267d028c31595bf581c909ee0d860454e2c59
Prasanth Krishnan <knp281192@gmail.com>
2015-11-04 04:00:43 -0600

    Adds print for cont iter

--- sankalpa_fsc.py
+++ sankalpa_fsc.py
@@ -318,7 +318,8 @@ class Xmp(Fuse):
         def update_remote_file(self):
             print '********** update_remote_file ************'
             content_iter = self.read_file_contents()
-            print '********** update_remote_file content_iter ************ %s ' % content_iter
+            for i in content_iter:
+                print '********** update_remote_file content_iter ************ %s ' % content_iter.content
             ack = stub.update_file(content_iter, _TIMEOUT_SECONDS)
             if ack.file_path != self.path or ack.num_bytes != os.fstat(self.fd).st_size:
                print '********** File Update Error ************'
